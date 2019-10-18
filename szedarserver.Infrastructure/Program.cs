5a67942a5478c2e3b054d318ea8fd12c846304ed
matszc <matiz305@hotmail.com>
2019-10-18 14:16:25 +0200

    inital

---
+++ szedarserver.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace szedarserver.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
